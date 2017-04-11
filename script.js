5a85bb9cc1a0c5a6c474b86e4a56bc6e740e42d1
Yuri <moluco99@gmail.com>
2017-04-12 01:12:12 +0200

    Estanciar la clase Juego

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 let canvas = document.getElementById("canvas")
 let c = canvas.getContext('2d')
 
-function init() {
 
+// function init() {
+    // let game = new Game(0,0,600,600)
     // let game = new Game(0,0,600,500)
 
     // var player = new Player(20, 20, 10, 10)
     // player.draw()
 
-}
+// }
 
 class Player {
     constructor(x, y, width, height) {
@@ -26,11 +27,14 @@ class Player {
 }
 
 class Game {
-    constructor(x,y,width,height) {
+    constructor(x, y, width, height) {
         canvas.width = width
         canvas.height = height
         c.fillStyle = "black"
         c.fillRect(0, 0, width, height);
+
+        let player = new Player(50,50,10,10)
+        player.draw()
     }
 
     update() {
@@ -38,7 +42,9 @@ class Game {
     }
 
     draw() {
-        
+
 
     }
 }
+
+let game = new Game(0,0,600,600)
