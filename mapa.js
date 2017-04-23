043287829ad51ede702ee03349075d5741ea81bf
Yuri <moluco99@gmail.com>
2017-04-24 00:30:39 +0200

    Trabajando en dibujar el mapa. Hay problemas con el image.onload

--- mapa.js
+++ mapa.js
@@ -14,39 +14,55 @@ function init(){
  canvas.height = getMapHeightInTiles() * getTileHeight()
  cont = canvas.getContext("2d")
 
- dibujarMapa()
+ var wall = new Image()
+ wall.src = "assets/wallside.png"
+ wall.onload = function(){
+    
+    cont.drawImage(wall,0,0)
+
+ }
+
+//  dibujarMapa()
 }
 
 function dibujarMapa(){
         var posX = 0
         var posY = 0
-        var count = 0
+        var count = 1
+
+        var tile1 = new Image()
+        var tile2 = new Image()
+        tile1.src = "assets/wallside.png"
+        tile2.src = "assets/marbletile.png"
 
     for (var i = 0; i < world.layers[0].data.length; i++) {
-        count++
-        console.log("hola: "+ count)
-        var tile = new Image()
         
-        tile.onload = function(){
-        console.log("mayor")
-        if(i == 2){
-            tile.src = "assets/marbletile.png"
-        }
-        if(i == 1){
-            tile.src = "assets/wallside.png"
+        console.log(count)
+         if(world.layers[0].data[i] == 2){
+        cont.drawImage(tile2,posX,posY)
+            
+         }
+         if(world.layers[0].data[i] == 1){
+           cont.drawImage(tile1,posX,posY)
+          }
+
+        console.log("posX:" + posX)
+        console.log("posY:" + posY)
+        console.log("")
+   
+        if(count <= getMapWidthInTiles()){
+            posX += getTileWidth()
         }
         if(count > getMapWidthInTiles()){
-            count = 0
-            posX = 0
-            posY += getTileHeight()
-        }else{
-            posX += getTileWidth()
-            posY = 0 
-        }
+         count = 1
+         posX = 0
+         posY += getTileHeight()
+     }
+      count++
+      
+ }
 
-        cont.drawImage(tile,posX,posY)
-        }
-    }
+    
 }
 
 
