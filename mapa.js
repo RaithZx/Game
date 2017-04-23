7b473224535a3b78f72f9bbb7eedc155a45f6544
Yuri <moluco99@gmail.com>
2017-04-23 21:21:56 +0200

    Dinujar mapa y repositorio mapa

--- mapa.js
+++ mapa.js
@@ -1,12 +1,55 @@
 window.addEventListener("load", init())
+
+// var mapHeight
+// var mapWidth 
+// var canvas 
+// var cont 
+
 function init(){
-console.log(world)
+ mapHeight = getMapHeightInTiles()
+ mapWidth = getMapWidthInTiles()
+
+ canvas = document.getElementById("canvas")
+ canvas.width = getMapWidthInTiles() * getTileWidth()
+ canvas.height = getMapHeightInTiles() * getTileHeight()
+ cont = canvas.getContext("2d")
+
+ dibujarMapa()
 }
 
-let canvas = document.getElementById("canvas")
-let cont = canvas.getContext("2d")
+function dibujarMapa(){
+        var posX = 0
+        var posY = 0
+        var count = 0
 
-// canvas.width = mapWidth
-// canvas.height = mapHeight
+    for (var i = 0; i < world.layers[0].data.length; i++) {
+        count++
+        console.log("hola: "+ count)
+        var tile = new Image()
+        
+        tile.onload = function(){
+        console.log("mayor")
+        if(i == 2){
+            tile.src = "assets/marbletile.png"
+        }
+        if(i == 1){
+            tile.src = "assets/wallside.png"
+        }
+        if(count > getMapWidthInTiles()){
+            count = 0
+            posX = 0
+            posY += getTileHeight()
+        }else{
+            posX += getTileWidth()
+            posY = 0 
+        }
+
+        cont.drawImage(tile,posX,posY)
+        }
+    }
+}
+
+
+
 
 
--- repositorioWorld.js
+++ repositorioWorld.js
@@ -1,23 +1,23 @@
 function getMapHeightInTiles(){
-    
+    return world.height
 }
 
 function getMapWidthInTiles(){
-
+    return world.width
 }
 
 function getTileWidth(){
-
+    return world.tilewidth
 }
 
 function getTileHeight(){
+    return world.tileheight
+}
+
+function getMapWidthInPx(){
 
 }
 
-function getMapWidth(){
-
-}
-
-function getMapHeight(){
+function getMapHeightInPx(){
 
 }
